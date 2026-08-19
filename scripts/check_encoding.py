f687fa9b50fdc07843af595c214f34999a85b547
Cipas <shiroi1229@gmail.com>
2026-08-20 08:45:06 +0900

    security: remove hardcoded DB password from encoding check

--- scripts/check_encoding.py
+++ scripts/check_encoding.py
@@ -1,3 +1,15 @@
+import os
+
 import psycopg2
-conn=psycopg2.connect(host='172.25.208.1', port=5432, dbname='ssp_memory', user='ssp_admin', password='Mizuho0824')
+from dotenv import load_dotenv
+
+load_dotenv()
+conn = psycopg2.connect(
+    host=os.getenv("POSTGRES_HOST", "172.25.208.1"),
+    port=int(os.getenv("POSTGRES_PORT", "5432")),
+    dbname=os.getenv("POSTGRES_DB", "ssp_memory"),
+    user=os.getenv("POSTGRES_USER", "ssp_admin"),
+    password=os.environ["POSTGRES_PASSWORD"],
+)
 print(conn.encoding)
+conn.close()
