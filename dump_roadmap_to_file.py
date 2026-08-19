4bcfc3d1d00299f8c3fefa32558322c0adb97be7
Cipas <shiroi1229@gmail.com>
2026-08-20 08:44:53 +0900

    security: remove hardcoded DB password from roadmap dump

--- dump_roadmap_to_file.py
+++ dump_roadmap_to_file.py
@@ -1,10 +1,22 @@
-﻿import psycopg2
 import json
+import os
+
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+DB_CONFIG = {
+    "host": os.getenv("POSTGRES_HOST", "172.25.208.1"),
+    "port": int(os.getenv("POSTGRES_PORT", "5432")),
+    "dbname": os.getenv("POSTGRES_DB", "ssp_memory"),
+    "user": os.getenv("POSTGRES_USER", "ssp_admin"),
+    "password": os.environ["POSTGRES_PASSWORD"],
+}
 
 LIST_FIELDS = ["keyFeatures", "dependencies", "metrics"]
 STRING_FIELDS = ["owner", "documentationLink", "prLink", "startDate", "endDate", "development_details"]
 
-with psycopg2.connect(host='172.25.208.1', dbname='ssp_memory', user='ssp_admin', password='Mizuho0824') as conn:
+with psycopg2.connect(**DB_CONFIG) as conn:
     with conn.cursor() as cur:
         cur.execute("""
             SELECT version, codename, goal, status, progress, description,
